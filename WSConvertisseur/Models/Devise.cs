950659f09d8d9deb61f804cd9700958e1633f899
Maxime Brossard <maxime.brossard5@gmail.com>
2024-09-19 15:35:17 +0200

    feat: adding devise

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,31 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+        private string nomDevise;
+
+		public string NomDevise
+		{
+			get { return nomDevise; }
+			set { nomDevise = value; }
+		}
+
+		private double taux;
+
+		public double Taux
+		{
+			get { return taux; }
+			set { taux = value; }
+		}
+
+
+	}
+}
