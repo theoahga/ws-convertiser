1b6613cb47944e61a6e4d034a4c0482df7acfe04
Maxime Brossard <maxime.brossard5@gmail.com>
2024-09-20 14:27:14 +0200

    feat: unit test

---
+++ WSConvertisseurTests/Controllers/DevisesControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSConvertisseur.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WSConvertisseur.Models;
+
+namespace WSConvertisseur.Controllers.Tests
+{
+    [TestClass()]
+    public class DevisesControllerTests
+    {
+        [TestMethod()]
+        public void GetDevises_ExistingIdPassed_ReturnsRightItem()
+        {
+            // Arrange
+            DevisesController controller = new DevisesController();
+            // Act
+            var result = controller.GetById(1);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Devise>), "Pas un ActionResult"); 
+            Assert.IsNull(result.Result, "Erreur est pas null");
+            Assert.IsInstanceOfType(result.Value, typeof(Devise), "Pas une Devise");
+            Assert.AreEqual(new Devise(1, "Dollar", 1.08), (Devise?)result.Value, "Devises pas identiques");
+        }
+    }
+}
